f5e595c65066264743481cf73f86ababa06c9a5a
Bartłomiej Kocot <38502616+bartekxk@users.noreply.github.com>
2019-10-09 15:09:53 +0200

    Final fix to bootloader.

--- bootloader.asm
+++ bootloader.asm
@@ -1,22 +1,19 @@
-BITS 16
+[BITS 16]
 org 0x7C00
+
 read_kernel:
-    mov si, 0x0
 
-    mov ax, 0x1000   ; setting up the address to read into
-    mov es, ax       ; moving the value in to es
-    xor bx, bx       ; clearing bx
-    mov ah, 0x02     ; floppy function
-    mov al, 1        ; read 1 sector
-    mov ch, 0        ; cylinder
-    mov cl, 2        ; sector to read
-    mov dh, 0        ; head number
-    mov dl, 0        ; drive number
+        mov ah, 0x02 ; Read sectors from floppy drive
+        mov al, 4    ; Read 4 sector
+        mov ch, 0    ; Cylinder 0
+        mov cl, 2    ; Sector 2
+        mov dh, 0    ; Head 0
+        mov bx, 0x0500 
+        mov es, bx   
+        xor bx, bx   
+        int 0x13
 
-    int 0x13         ; BIOS Interrupt Call
+        jmp 0x0500:0
 
-    jc read_kernel
-    jmp  0x1000:0 
-
-times 510 - ($-$$) db 0
-dw 0xAA55
+times   510-($-$$) db 0  
+dw      0xaa55              
