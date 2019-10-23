5fed0b0a89ca13c0cce9264bd66ff6c6f3e3aea1
bartekxk <bartekkocot@op.pl>
2019-10-23 23:04:22 +0200

    Add movement controller for longer than 1 pixel snake

--- kernel.asm
+++ kernel.asm
@@ -72,22 +72,34 @@ case_a2:
 _game_snake:
 	pusha
 	call _load_strings_to_ram
-	mov byte [snake_x], 40
-	mov byte [snake_y], 12
+	call _clear_screen
+	call _draw_board
 	mov word [apples_x], 0
 	mov word [apples_x+2], 0
 	mov word [apples_y], 0
 	mov word [apples_x+2],0
+	mov byte [snake_len],3
+	sub sp,2002
+	mov [snake_memory],sp
+	add word [snake_memory],2
+	mov si,[snake_memory]
+	mov byte [si],40
+	mov byte [si+1],12
+	mov byte [si+2],40
+	mov byte [si+3],13
+	mov byte [si+4],40
+	mov byte [si+5],14
+	mov byte [si-2],40
+	mov byte [si-1],15
 repeat_gs1:
-	call _clear_screen
-	call _draw_board
 	call _move_snake
 	call _rand_and_draw_apples
 	call _keyboard_game_support
-	cmp ah, 01h
+	cmp ah, -1
 	je done_gs1
 	jmp repeat_gs1			; infinite loop
 done_gs1:
+	add sp,2002
 	popa
 	ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;_game_snake;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -285,6 +297,16 @@ _get_key:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;_keyboard_game_support;;;;;;;
 _keyboard_game_support:
 	call _get_key
+	mov si,[snake_memory]
+	movzx bx,byte[snake_len]
+	sub bl,1
+	shl bl,1
+	mov dh,[si+bx]
+	mov dl,[si+bx+1]
+	mov [si-2],dh
+	mov [si-1],dl
+	mov dh,[si]
+	mov dl,[si+1]
 	cmp ah,04Bh
 	je left
     cmp ah,04Dh
@@ -295,37 +317,70 @@ _keyboard_game_support:
 	je down
 	cmp ah,013h
 	je restart
-	jmp done_gt1
+	cmp ah,01h
+	je esc
+	jmp nothing_to_do
 left:
-	cmp byte [snake_x],1
-	je done_gt1
-	sub byte [snake_x],1
+	cmp dh,1
+	je nothing_to_do
+	sub dh,1
     jmp done_gt1
 right:
-	cmp byte [snake_x],78
-	je done_gt1
-	add byte [snake_x],1
+	cmp dh,78
+	je nothing_to_do
+	add dh,1
 	jmp done_gt1
 up:
-	cmp byte [snake_y],1
-	je done_gt1
-	sub byte [snake_y],1
+	cmp dl,1
+	je nothing_to_do
+	sub dl,1
 	jmp done_gt1
 down:
-	cmp byte [snake_y],22
-	je done_gt1
-	add byte [snake_y],1
+	cmp dl,22
+	je nothing_to_do
+	add dl,1
+	jmp done_gt1
+esc:
+	mov ah,-1
+	jmp nothing_to_do
 restart:
 ;;;todo
-	jmp done_gt1
+	jmp nothing_to_do
 done_gt1:
 
+	movzx bx,[snake_len]
+	dec bx
+	shl bx,2
+repeat_gt2:
+	cmp bx,0
+	je done_gt2
+
+	mov ah,[si+bx-2]
+	mov al,[si+bx-1]
+	mov [si+bx],ah
+	mov [si+bx+1],al
+	sub bx,2
+	jmp repeat_gt2
+
+done_gt2:
+ 	mov [si],dh
+ 	mov [si+1],dl
+ 	jmp return_gt2
+nothing_to_do:
+	mov byte [si-2],-1
+	mov byte [si-1],-1
+return_gt2:
 	ret
 ;;;;;;;;;;;;;;;;;;;;;;;;_keyboard_game_support;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;_move_snake;;;;;;;;;;;;;;;;;;;;;;;;;
 _move_snake:
 	pusha
-
+	movzx cx,byte [snake_len]
+	mov si,[snake_memory]
+repeat_ms1:
+	cmp cx,0
+	je done_ms1
+	push cx
 	mov cx,1
 	mov bp, snake
 	mov ax, ds
@@ -333,10 +388,29 @@ _move_snake:
 	mov ah, 13h        ;SERVICE TO DISPLAY STRING WITH COLOR.
 	mov bh, 0          ;PAGE (ALWAYS ZERO).
 	mov bl, 00010000b
-	mov dl, [snake_x]  
-	mov dh, [snake_y]  
+	mov dl, [si]  
+	mov dh, [si+1]  
 	int 10h           ;BIOS SCREEN SERVICES.  
+	add si,2
+	pop cx
+	dec cx
+	jmp repeat_ms1
 
+done_ms1:
+	mov si,[snake_memory]
+	mov cx,1
+	mov bp, white_sign
+	mov ax, ds
+	mov es, ax        
+	mov ah, 13h        ;SERVICE TO DISPLAY STRING WITH COLOR.
+	mov bh, 0          ;PAGE (ALWAYS ZERO).
+	mov bl, 0
+	mov dl, [si-2]  
+	mov dh, [si-1]  
+	cmp dl, -1
+	je done_ms2
+	int 10h           ;BIOS SCREEN SERVICES.  
+done_ms2:
 	popa
 	ret
 ;;;;;;;;;;;;;;;;;;;;;;;_move_snake;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -369,10 +443,6 @@ repeat_da1:
 	inc di
 	jmp repeat_da1
 done_da1:
-	mov si,apples_y
-	call _print_str
-	mov si, apples_x
-	call _print_str
 	popa
 	ret
 ;;;;;;;;;;;;;;;;;;;;;;;_draw_apples;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -389,12 +459,13 @@ section .data
 	app1 db 'Snake[1]',10,13,0
 	back db 'Back[B]',10,13,0
 	next_line_str db 10,13,0
-	snake_x db 40
-	snake_y db 12
+	snake_memory dw 0
 	time db 0,0
 	snake db 'x'
+	snake_len db 3
 	snake_legend db 'Arrow keys, esc to exit.',0
 	rand_num db 1,1,0
+	white_sign db ' '
 	apple db 'o'
 	apples_x db -1,-1,0
 	apples_y db -1,-1,0
